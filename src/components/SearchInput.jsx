ff7ac59610b62843a2233ec4d703cca6c813660f
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-12 14:30:28 +0700

    products

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,111 +1,23 @@
-// import React, { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { searchAPI } from "../store/slice/Db";
-// import { styled } from "@mui/material";
-// import SearchIcon from "@mui/icons-material/Search";
-
-// function SearchInput() {
-//   const [click, setClick] = useState(false);
-//   const [query, setQuery] = useState("");
-//   const dispatch = useDispatch();
-
-//   const onClick = () => {
-//     setClick(!click);
-//     if (query) {
-//       dispatch(searchAPI(query));
-//     }
-//   };
-
-//   const handleChange = (e) => setQuery(e.target.value);
-
-//   return (
-//     <WrapperAll>
-//       <input
-//         className={click ? "open" : "close"}
-//         placeholder="search"
-//         value={query}
-//         onChange={handleChange}
-//       />
-//       <SearchIcon className="search_icon" onClick={onClick} />
-//     </WrapperAll>
-//   );
-// }
-// export default SearchInput;
-// const WrapperAll = styled("div")`
-//   position: relative;
-//   display: flex;
-//   align-items: center;
-//   .search_icon {
-//     position: absolute;
-//     right: 0px;
-//     cursor: pointer;
-//     width: 40px;
-//     height: 40px;
-//     border: 1px solid grey;
-//     padding: 5px;
-//     border-radius: 5px;
-//   }
-//   .open {
-//     width: 0px;
-//     height: 40px;
-//     transition-duration: 1s;
-//     width: 250px;
-//     height: 40px;
-//     max-width: 100%;
-//     outline: none;
-//     border: 1px solid blue;
-//     padding-left: 10px;
-//     font-size: 18px;
-//     border-radius: 8px;
-//   }
-//   .close {
-//     width: 250px;
-//     height: 40px;
-//     transition-duration: 1s;
-//     width: 0px;
-//     height: 40px;
-//     border-radius: 8px;
-//     outline: none;
-//     border: 1px solid white;
-//   }
-// `;
-
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { getAll, searchAPI } from "../store/slice/Db";
+import { searchAPI } from "../store/slice/Db";
 import { styled } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { useLocation, useNavigate } from "react-router-dom";
 
 function SearchInput() {
   const [click, setClick] = useState(false);
   const [query, setQuery] = useState("");
-  const nav = useNavigate();
-  const loc = useLocation();
-
-  const [showResults, setShowResults] = useState(false);
   const dispatch = useDispatch();
 
   const onClick = () => {
     setClick(!click);
-    nav("/?query=" + query);
-    dispatch(searchAPI(query));
-    setShowResults(true);
+    if (query) {
+      dispatch(searchAPI(query));
+    }
   };
 
   const handleChange = (e) => setQuery(e.target.value);
-  useEffect(() => {
-    if (!loc.search) {
-      setQuery("");
-      dispatch(getAll());
-    }
-  }, [loc.search]);
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      onClick();
-      setQuery("");
-    }
-  };
+
   return (
     <WrapperAll>
       <input
@@ -113,26 +25,19 @@ function SearchInput() {
         placeholder="search"
         value={query}
         onChange={handleChange}
-        onKeyDown={handleKeyDown}
       />
-
       <SearchIcon className="search_icon" onClick={onClick} />
-
-      {showResults && <div className="results"></div>}
     </WrapperAll>
   );
 }
-
 export default SearchInput;
-
 const WrapperAll = styled("div")`
   position: relative;
   display: flex;
   align-items: center;
-  flex-direction: column;
   .search_icon {
     position: absolute;
-    right: 50px;
+    right: 0px;
     cursor: pointer;
     width: 40px;
     height: 40px;
@@ -140,11 +45,12 @@ const WrapperAll = styled("div")`
     padding: 5px;
     border-radius: 5px;
   }
-
   .open {
-    width: 250px;
+    width: 0px;
     height: 40px;
     transition-duration: 1s;
+    width: 250px;
+    height: 40px;
     max-width: 100%;
     outline: none;
     border: 1px solid blue;
@@ -152,22 +58,14 @@ const WrapperAll = styled("div")`
     font-size: 18px;
     border-radius: 8px;
   }
-
   .close {
-    width: 0px;
+    width: 250px;
     height: 40px;
     transition-duration: 1s;
+    width: 0px;
+    height: 40px;
     border-radius: 8px;
     outline: none;
     border: 1px solid white;
-  }
-
-  .results {
-    margin-top: 20px;
-    padding: 10px;
-    background-color: #f1f1f1;
-    border-radius: 8px;
-    width: 100%;
-    max-width: 400px;
   }
 `;
--- src/store/slice/Db.js
+++ src/store/slice/Db.js
@@ -21,7 +21,7 @@ export const findById = createAsyncThunk("data/findById", async (id) => {
     return await fetch("http://10.10.10.12:9091/product/findById/" + id, {
       method: "GET",
       headers: {
-        "Content-Type": "application/json", 
+        "Content-Type": "application/json",
       },
     })
       .then((response) => response.json())
@@ -94,3 +94,114 @@ const Db = createSlice({
 });
 export const DataBase = Db.actions;
 export default Db;
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+// // Бардык продуктыларды алуу
+// export const getAll = createAsyncThunk(
+//   "data/getAll",
+//   async (_, { rejectWithValue }) => {
+//     try {
+//       const response = await fetch("http://10.10.10.12:9091/product/getAll", {
+//         method: "GET",
+//         headers: { "Content-Type": "application/json" },
+//       });
+
+//       if (!response.ok) {
+//         throw new Error("Серверден ката келип чыкты!");
+//       }
+
+//       return await response.json();
+//     } catch (error) {
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// // Белгилүү бир продуктуну алуу
+// export const findById = createAsyncThunk(
+//   "data/findById",
+//   async (id, { rejectWithValue }) => {
+//     try {
+//       const response = await fetch(
+//         `http://10.10.10.12:9091/product/findById/${id}`,
+//         {
+//           method: "GET",
+//           headers: { "Content-Type": "application/json" },
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error("Серверден ката келип чыкты!");
+//       }
+
+//       return await response.json();
+//     } catch (error) {
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// // Издөө функциясы
+// export const searchAPI = createAsyncThunk(
+//   "data/searchAPI",
+//   async (value, { rejectWithValue }) => {
+//     try {
+//       const response = await fetch(
+//         `http://10.10.10.12:9091/product/search?query=${value}`,
+//         {
+//           method: "GET",
+//           headers: { "Content-Type": "application/json" },
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error("Издөө катасы!");
+//       }
+
+//       return await response.json();
+//     } catch (error) {
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// // Redux Slice
+// const Db = createSlice({
+//   name: "data",
+//   initialState: {
+//     data: [],
+//     find_db: {},
+//     search_result: [],
+//     error: null,
+//     status: "idle",
+//   },
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(getAll.pending, (state) => {
+//         state.status = "loading";
+//       })
+//       .addCase(getAll.fulfilled, (state, action) => {
+//         state.status = "succeeded";
+//         state.data = action.payload;
+//       })
+//       .addCase(getAll.rejected, (state, action) => {
+//         state.status = "failed";
+//         state.error = action.payload;
+//         state.data = [];
+//       })
+//       .addCase(findById.fulfilled, (state, action) => {
+//         state.find_db = action.payload;
+//       })
+//       .addCase(searchAPI.fulfilled, (state, action) => {
+//         state.search_result = action.payload;
+//       })
+//       .addCase(searchAPI.rejected, (state, action) => {
+//         state.error = action.payload;
+//         state.search_result = [];
+//       });
+//   },
+// });
+
+// export const DataBase = Db.actions;
+// export default Db;
